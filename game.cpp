8793e035574b3f426326327579c7585e39561e01
FatimaFaisal1796 <fatimafaisal1796@gmail.com>
2026-05-03 20:45:16 +0500

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,10 +1,12 @@
 #include "game.h"
 #include <iostream>
 using namespace std;
+
 Game::Game()
 {
     isWhiteTurn = true;
 }
+
 void Game::start() 
 {
     while (true)
@@ -23,15 +25,18 @@ void Game::start()
         int sx, sy, ex, ey;
         cin >> sx >> sy >> ex >> ey;
 
-        if (board.movePiece(sx, sy, ex, ey)) {
+        if (board.movePiece(sx, sy, ex, ey)) 
+        {
             switchTurn();
         }
-        else {
+        else 
+        {
            cout << "Invalid move!\n";
         }
     }
 }
 
-void Game::switchTurn() {
+void Game::switchTurn()
+{
     isWhiteTurn = !isWhiteTurn;
 }
