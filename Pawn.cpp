8835f4c52129b71555c00a83ad79d67e2b404e4e
FatimaFaisal1796 <fatimafaisal1796@gmail.com>
2026-05-14 23:37:03 +0500

    Updated Pawn

--- Pawn.cpp
+++ Pawn.cpp
@@ -6,13 +6,11 @@ bool Pawn::isValidMove(int x1, int y1, int x2, int y2)
 {
     if (color == 'W')
     {
-        return (x2 == x1 - 1 && y1 == y2) ||
-            (x1 == 6 && x2 == 4 && y1 == y2); 
+        return (x2 == x1 - 1 && y1 == y2) || (x1 == 6 && x2 == 4 && y1 == y2); 
     }
     else
     {
-        return (x2 == x1 + 1 && y1 == y2) ||
-            (x1 == 1 && x2 == 3 && y1 == y2);
+        return (x2 == x1 + 1 && y1 == y2) || (x1 == 1 && x2 == 3 && y1 == y2);
     }
 }
 
